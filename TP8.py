9d96c008ae4f429726f6c1a4d04bbd17ce48d342
Arthur <arthurpairaud@gmail.com>
2020-03-26 17:38:04 +0100

    ajout de la qualité du code

--- TP8.py
+++ TP8.py
@@ -9,26 +9,93 @@ if __name__ == "__main__":
         erreur = 0
         warning = 0
         tests = 0
+        qualite = 0
+
         print(eleve)
-        s = Popen(["gcc", "-ansi", "-Wall", "-o", eleve[:-2], "./Rendus_eleves/eleves_bis/" + eleve], stdout = PIPE, stderr = PIPE, encoding = "utf8")
-        s.wait()
-        if s.returncode != 0:
+        compil = Popen(["gcc", "-ansi", "-Wall", "-o", eleve[:-2], "./Rendus_eleves/eleves_bis/" + eleve], stdout = PIPE, stderr = PIPE, encoding = "utf8")
+        compil.wait()
+        if compil.returncode != 0:
             erreur = 1
-        for ligne in s.stderr:
+        for ligne in compil.stderr:
             if 'warning' in ligne:
                 warning += 1
 
         print("erreur :", erreur, "\nwarning :", warning)
 
         if erreur == 0:
-            out = call(["./" + eleve[:-2], "0", "0"], stdout = sortie, encoding = "utf8")
-            if(sortie == "La somme de 0 et 0 vaut 0\n"):
-                tests += 1
+            out = Popen(["./" + eleve[:-2], "0", "0"], stdout = PIPE, encoding = "utf8")
+            out.wait()
 
-            out = call(["./" + eleve[:-2], "1", "0"], encoding = "utf8")
-            if(out == "La somme de 1 et 0 vaut 1\n"):
-                tests += 1
+            for sortie in out.stdout:
+                if(sortie == "La somme de 0 et 0 vaut 0\n"):
+                    tests += 1
+            
 
-        print("test : ", tests)
+            out = Popen(["./" + eleve[:-2], "1", "0"], stdout = PIPE, encoding = "utf8")
+            out.wait()
+
+            for sortie in out.stdout:
+                if(sortie == "La somme de 1 et 0 vaut 1\n"):
+                    tests += 1
+
+
+            out = Popen(["./" + eleve[:-2], "0", "1"], stdout = PIPE, encoding = "utf8")
+            out.wait()
+
+            for sortie in out.stdout:
+                if(sortie == "La somme de 0 et 1 vaut 1\n"):
+                    tests += 1
+            
+
+            out = Popen(["./" + eleve[:-2], "1", "1"], stdout = PIPE, encoding = "utf8")
+            out.wait()
+
+            for sortie in out.stdout:
+                if(sortie == "La somme de 1 et 1 vaut 2\n"):
+                    tests += 1
+            
+
+            out = Popen(["./" + eleve[:-2], "12", "12"], stdout = PIPE, encoding = "utf8")
+            out.wait()
+
+            for sortie in out.stdout:
+                if(sortie == "La somme de 12 et 12 vaut 24\n"):
+                    tests += 1
+
+
+            out = Popen(["./" + eleve[:-2], "12", "-43"], stdout = PIPE, encoding = "utf8")
+            out.wait()
+
+            for sortie in out.stdout:
+                if(sortie == "La somme de 12 et -43 vaut -31\n"):
+                    tests += 1
+            
+
+            out = Popen(["./" + eleve[:-2], "-1", "-52"], stdout = PIPE, encoding = "utf8")
+            out.wait()
+
+            for sortie in out.stdout:
+                if(sortie == "La somme de -1 et -52 vaut -53\n"):
+                    tests += 1
+
+        print("tests : ", tests)
+
+        quali = Popen(["cat", "./Rendus_eleves/eleves_bis/" + eleve], stdout = PIPE, encoding = "utf8")
+        quali.wait()
+
+        tmp = 0
+        for ligne in quali.stdout:
+            if "/*" in ligne:
+                tmp = 1
+            if "*/" in ligne:
+                qualite += tmp
+                tmp = 0
+
+            if tmp != 0:
+                tmp += 1
+
+        print("qualite : " + str(qualite)) 
+
+        print('\n')
         if(erreur == 0):
             call(["rm", eleve[:-2]])
